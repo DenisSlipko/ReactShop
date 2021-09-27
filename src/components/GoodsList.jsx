e9a30644d346c4271989279c72b828f63cf364bb
Denis Slipko <denisslipko@MacBook-Pro-Denis.local>
2021-09-27 21:06:35 +0300

    load the project

---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,20 @@
+import { GoodsItem } from "./GoodsItem";
+import { useContext } from "react";
+import { ShopContext } from "../context";
+function GoodsList() {
+  const { goods = [] } = useContext(ShopContext);
+
+  if (!goods.length) {
+    return <h3>Nothing here</h3>;
+  }
+
+  return (
+    <div className="goods">
+      {goods.map((item) => (
+        <GoodsItem key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
+
+export { GoodsList };
